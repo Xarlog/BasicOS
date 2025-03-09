17bd3475d5835fad70303b1b829a08c3ccd3239f
Xarlog <polska.kamil.polska@interia.pl>
2025-03-09 17:56:00 +0100

    Moved real-mode printing to separate file.

--- boot/boot_sect.asm
+++ boot/boot_sect.asm
@@ -12,48 +12,8 @@ call f_load_kernel
 call switch_to_protected_mode
 jmp $;end loop
 
-;
-f_print:
-    push ax
-l_print_loop:
-    mov al, [bx]
-    int 0x10 ;print al
-    add bx, 1
-    cmp byte [bx], byte 0
-    jne l_print_loop
-    pop ax
-    ret
-;f_print_hex bl=value to print
-f_print_hex:
-    push bx
-    shr bl, 4 ;select 4 ms bits
-    push ax
-    mov al, hex_vals
-    add bl,al
-    mov al, [bx]
-    int 0x10
-    pop ax
-    pop bx
-    push bx
-    and bl,00001111b
-    push ax
-    mov al, hex_vals
-    add bl,al ;select 4 ls bits
-    mov al, [bx]
-    int 0x10
-    pop ax
-    pop bx
-    ret
-hex_vals:
-    db "0123456789ABCDEF"
-;
-f_ping:
-    pusha
-    popa
-    mov ah, 0x0e
-    mov al, 'P'
-    int 0x10
-    ret
+%include "boot/f_print.asm"
+
 greeting:
     db "Greetings from 16bit real mode! This is an absurdly simplistic and useless operating system." ,0xa,0xd,0
 greeting_pm:
---
+++ boot/f_print.asm
@@ -0,0 +1,41 @@
+f_print:
+    push ax
+l_print_loop:
+    mov al, [bx]
+    int 0x10 ;print al
+    add bx, 1
+    cmp byte [bx], byte 0
+    jne l_print_loop
+    pop ax
+    ret
+;f_print_hex bl=value to print
+f_print_hex:
+    push bx
+    shr bl, 4 ;select 4 ms bits
+    push ax
+    mov al, hex_vals
+    add bl,al
+    mov al, [bx]
+    int 0x10
+    pop ax
+    pop bx
+    push bx
+    and bl,00001111b
+    push ax
+    mov al, hex_vals
+    add bl,al ;select 4 ls bits
+    mov al, [bx]
+    int 0x10
+    pop ax
+    pop bx
+    ret
+hex_vals:
+    db "0123456789ABCDEF"
+;
+f_ping:
+    pusha
+    popa
+    mov ah, 0x0e
+    mov al, 'P'
+    int 0x10
+    ret
